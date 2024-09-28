a9d27c4bdb28b3144b6c2635d8af51c05848a634
johch3n611u <sjmjgd1165@gmail.com>
2024-09-29 01:33:44 +0800

    move path 參數控制產生檔案路徑

--- AngularSchematicsDemo/src/angular-schematics-demo/index.ts
+++ AngularSchematicsDemo/src/angular-schematics-demo/index.ts
@@ -1,4 +1,4 @@
-import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import { apply, mergeWith, move, Rule, SchematicContext, template, Tree, url } from '@angular-devkit/schematics';
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
@@ -9,7 +9,22 @@ import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 
 export function angularSchematicsDemo(_options: any): Rule {
   // _options：包含下的參數指令、別的 Schematics 所傳入的參數，可以使用 JSON Schema 驗證確保具有適當的預設值與類型
-  return (tree: Tree, _context: SchematicContext) => {
+  return (
+    tree: Tree, 
+    _context: SchematicContext
+  ) => {
+
+    const sourceTemplates = url('./templates'); // 使用範本
+
+    const sourceParametrizedTemplates = apply(sourceTemplates, [
+      template({
+        ..._options, // 使用者所輸入的參數
+      }),
+      move(`./dist/${_options.name}`)  // Move files to the target path
+    ]);
+
+    return mergeWith(sourceParametrizedTemplates);
+
     // 產生一個檔案名稱為使用者所輸入的 --name 參數的值，沒輸入則為 'hello' 的檔案，檔案內容為 'world'
     tree.create(_options.name || 'hello', 'world');
     return tree;
