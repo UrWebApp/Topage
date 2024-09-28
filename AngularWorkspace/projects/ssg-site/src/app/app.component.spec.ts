f43a1e8db9563a5475c54ae63c67241adf410ff2
DESKTOP-QSEF0D3\大哥 <carl12331@gmail.com>
2024-09-28 13:24:29 +0800

    fix: init

--- AngularWorkspace/projects/ssg-site/src/app/app.component.spec.ts
+++ AngularWorkspace/projects/ssg-site/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'SSG-Site' title`, () => {
+  it(`should have the 'ssg-site' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('SSG-Site');
+    expect(app.title).toEqual('ssg-site');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, SSG-Site');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ssg-site');
   });
 });
