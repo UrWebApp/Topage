50b2ac006bfdb439b14518a9fd4ef061a2cc66ba
kai98k <carl12331@gmail.com>
2024-10-25 21:40:54 +0800

    feat: add route

---
+++ AngularWorkspace/projects/ssg-site/src/app/article/article.routes.ts
@@ -0,0 +1,8 @@
+import { Routes } from '@angular/router';
+import { ArticleComponent } from './article.component';
+
+const articleRoutes: Routes = [
+  { path: 'article', component: ArticleComponent }
+];
+
+export default articleRoutes;
